51a973bedacd93be5c3775f75d02f0ec1e1e6a80
prijindal <priyanshujindal1995@gmail.com>
2015-09-09 05:35:33 +0530

    Gulp production enabled

---
+++ app/gulp/production.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat');
+
+gulp.task('sass', function() {
+    return gulp.src(assets.scss.source)
+            .pipe(sass({compressed:true}))
+            .pipe(gulp.dest(assets.scss.dest))
+})
+
+gulp.task('js', function() {
+  return gulp.src(assets.js.source)
+    .pipe(concat(assets.js.dest.filename))
+    .pipe(gulp.dest(assets.js.dest.path))
+})
+
+gulp.task('vendor:js', function() {
+    return gulp.src(assets.vendorjs.source)
+            .pipe(concat(assets.vendorjs.dest.filename))
+            .pipe(gulp.dest(assets.vendorjs.dest.path))
+})
+
+gulp.task('build', ['sass', 'vendor:js', 'js'])
